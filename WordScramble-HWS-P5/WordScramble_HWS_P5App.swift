9b74e873f698eebc518a9842248a053af90e78c2
adedwi1808 <adedwip1808@gmail.com>
2022-10-14 14:24:24 +0700

    Initial Commit

---
+++ WordScramble-HWS-P5/WordScramble_HWS_P5App.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_HWS_P5App.swift
+//  WordScramble-HWS-P5
+//
+//  Created by Ade Dwi Prayitno on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_HWS_P5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
