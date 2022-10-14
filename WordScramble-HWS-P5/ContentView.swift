421504a7b496f3b6811e13be0663e138b9ddbf78
adedwi1808 <adedwip1808@gmail.com>
2022-10-14 21:45:04 +0700

    Feat: Menambahkan method untuk validasi text inputan user

--- WordScramble-HWS-P5/ContentView.swift
+++ WordScramble-HWS-P5/ContentView.swift
@@ -55,8 +55,32 @@ struct ContentView: View {
                 return
             }
         }
-        
         fatalError("Tidak berhasil menemukan start.txt pada app bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !useWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
     }
 }
 
